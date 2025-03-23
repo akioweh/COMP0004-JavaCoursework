9428e9a7b219e3834bd0311d6f1c3bc2966fd95e
akioweh <79527273+akioweh@users.noreply.github.com>
2025-03-23 04:38:00 +0000

    print stack traces properly to log

--- src/main/java/com/akioweh/comp0004javacoursework/main/Main.java
+++ src/main/java/com/akioweh/comp0004javacoursework/main/Main.java
@@ -18,13 +18,44 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 
-@SuppressWarnings("CallToPrintStackTrace")
 public class Main {
     private static final int DEFAULT_PORT = 80;
     private static final String DEFAULT_WEBAPP_DIR = "src/main/webapp/";
     private static final String DEFAULT_TARGET_CLASSES = "target/classes";
     private static final String WEB_INF_CLASSES = "/WEB-INF/classes";
     private static final String LOGFILE = "logfile.txt";
+
+    // run with embedded Tomcat
+    public static void main(String[] args) {
+        final Logger logger = initLogger();
+        final Path appDirectory = Paths.get(DEFAULT_WEBAPP_DIR);
+        final Path targetClassesDirectory = Paths.get(DEFAULT_TARGET_CLASSES);
+
+        assert Files.exists(appDirectory) : "Webapp directory does not exist: " + appDirectory;
+        assert Files.exists(targetClassesDirectory) : "Target classes directory does not exist: " + targetClassesDirectory;
+
+        final Tomcat tomcat = new Tomcat();
+
+        tomcat.setPort(DEFAULT_PORT);
+        tomcat.getConnector();
+        // add a shutdown hook to *eliminate* Tomcat when the JVM exits
+        Runtime.getRuntime().addShutdownHook(shutdownHook(tomcat, logger));
+
+        // tell it where to find the webapp
+        Context context = tomcat.addWebapp("", appDirectory.toAbsolutePath().toString());
+        WebResourceRoot resources = new StandardRoot(context);
+        resources.addPreResources(new DirResourceSet(resources, WEB_INF_CLASSES, targetClassesDirectory.toAbsolutePath().toString(), "/"));
+        context.setResources(resources);
+
+        try {
+            tomcat.start();
+            logger.info("Server started on port " + DEFAULT_PORT);
+            tomcat.getServer().await();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "Error starting server: ");
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
 
     private static @NotNull Logger initLogger() {
         Logger logger = Logger.getLogger(Main.class.getName());
@@ -48,42 +79,9 @@ public class Main {
                 app.destroy();
                 logger.info("Server closed.");
             } catch (Exception e) {
-                logger.log(Level.SEVERE, "Error closing server ", e);
-                e.printStackTrace();
+                logger.log(Level.SEVERE, "Error closing server:");
+                logger.log(Level.SEVERE, e.getMessage(), e);
             }
         });
-    }
-
-    // run with embedded Tomcat
-    public static void main(String[] args) {
-        final Logger logger = initLogger();
-        final Path appDirectory = Paths.get(DEFAULT_WEBAPP_DIR);
-        final Path targetClassesDirectory = Paths.get(DEFAULT_TARGET_CLASSES);
-
-        assert Files.exists(appDirectory) : "Webapp directory does not exist: " + appDirectory;
-        assert Files.exists(targetClassesDirectory) : "Target classes directory does not exist: " + targetClassesDirectory;
-
-        final Tomcat tomcat = new Tomcat();
-
-        tomcat.setPort(DEFAULT_PORT);
-        tomcat.getConnector();
-        // add a shutdown hook to *eliminate* Tomcat when the JVM exits
-        Runtime.getRuntime().addShutdownHook(shutdownHook(tomcat, logger));
-
-        // tell it where to find the webapp
-        Context context = tomcat.addWebapp("", appDirectory.toAbsolutePath().toString());
-        WebResourceRoot resources = new StandardRoot(context);
-        resources.addPreResources(new DirResourceSet(resources, WEB_INF_CLASSES,
-                targetClassesDirectory.toAbsolutePath().toString(), "/"));
-        context.setResources(resources);
-
-        try {
-            tomcat.start();
-            logger.info("Server started on port " + DEFAULT_PORT);
-            tomcat.getServer().await();
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "Error starting server ", e);
-            e.printStackTrace();
-        }
     }
 }
